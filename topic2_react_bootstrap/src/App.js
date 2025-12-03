cc36cbe0e378fa16c6c348118ffed028ea49e64c
thopn3 <thopn3@fpt.edu.vn>
2025-12-03 14:15:05 +0700

    Change Layout

--- topic2_react_bootstrap/src/App.js
+++ topic2_react_bootstrap/src/App.js
@@ -19,8 +19,8 @@ function App() {
     ]
   }
 
-  const {books, authors, publishers} = database;
-  
+  const { books, authors, publishers } = database;
+
   return (
     // JSX (Javascript Extends) syntax
     <div className='container-fluid'>
@@ -39,7 +39,7 @@ function App() {
           </ul>
         </div>
       </div>
-      
+
       {/* Content */}
       <div className='row'>
         <div className='d-none d-md-block col-md-2'>
@@ -50,8 +50,40 @@ function App() {
           </ul>
         </div>
         <div className='col-12 col-md-10'>
-          <h2>Create a new Book</h2>
-          
+          <form className='mb-3'>
+            <h2 className='text-center'>Create a new Book</h2>
+            <div className='mb-3'>
+              <label className='form-label'>BookId</label>
+              <input className='form-control' />
+            </div>
+            <div className='mb-3'>
+              <label className='form-label'>Title</label>
+              <input className='form-control' />
+            </div>
+            <div className='mb-3'>
+              <label className='form-label'>Authors</label><br />
+              {
+                authors?.map(a => (
+                  <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="checkbox" value={a?.auId} />
+                    <label class="form-check-label">{a.name}</label>
+                  </div>
+                ))
+              }
+            </div>
+            <div className='mb-3'>
+              <label className='form-label'>Publisher</label><br />
+              <select class="form-select">
+                <option selected>-- Choice a publisher --</option>
+                {
+                  publishers?.map(p => (
+                    <option value={p?.pubId}>{p?.name}</option>
+                  ))
+                }
+              </select>
+            </div>
+            <button className='btn btn-success'>Create</button>
+          </form>
         </div>
       </div>
 
